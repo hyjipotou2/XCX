d3a5b0177e8884af56df83731cc4a4e63316e696
chenfengqiannian <k564398853@163.com>
2017-08-30 16:38:41 +0800

    travis

--- XCX/settings.py
+++ XCX/settings.py
@@ -109,7 +109,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
