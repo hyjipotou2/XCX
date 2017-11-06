7d5aefe74df1f20c43ac364976b5a65287ab2f49
mac <k564398853@163.com>
2017-11-06 22:38:46 +0800

    d

--- static/userManagement/indexShow/js/main.js
+++ static/userManagement/indexShow/js/main.js
@@ -3,21 +3,30 @@
  * Created by mac on 2017/2/21.
  */
 $(document).ready(function(){
-
-
-    var heigt=$('.bannder').width()*575/1280;
+var heigt=$('.bannder').width()*554/1280;
     $('.bannder').height(heigt);
-
+    var height2=$('.first').width()*784/1280;
+    $('.first').height(height2);
+    var height3=$('.second').width()*636/1280;
+    $('.second').height(height3);
+     var height4=$('.third').width()*1282/1280;
+    $('.third').height(height4);
 
 
 });
 
 $(window).resize(function () {
-    var heigt=$('.bannder').width()*575/1280;
+    var heigt=$('.bannder').width()*554/1280;
     $('.bannder').height(heigt);
-    $('.bannder>img').height(heigt);
 
-    console.log($('.bannder>img'));
+   var height2=$('.first').width()*784/1280;
+    $('.first').height(height2);
+
+    var height3=$('.second').width()*636/1280;
+    $('.second').height(height3);
+    var height4=$('.third').width()*1282/1280;
+    $('.third').height(height4);
+    console.log(height3)
 
 })
 
