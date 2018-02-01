2d5c606747aaa941af3fc844dcb10c0000752efd
chenfgengqiannian <k564398853@163.com>
2018-02-01 13:26:45 +0800

    f

--- XCX/settings.py
+++ XCX/settings.py
@@ -108,5 +108,5 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
